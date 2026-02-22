94bcb968af1f672917662e496803a96c95366b69
Bruno Barreto <168922135+BrunoBarreto91@users.noreply.github.com>
2026-02-22 15:49:59 -0300

    patch: move directory

--- Jarvis_U.I/src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-import { cn } from "@/lib/utils"  // shadcn utils
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
